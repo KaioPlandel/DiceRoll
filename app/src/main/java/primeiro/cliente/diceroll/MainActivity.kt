b5c41dd7db5de72fad7556994d8a0057f9ca3b30
kaiop <kaioplandel@gmail.com>
2022-02-03 11:42:59 -0300

    update

--- app/src/main/java/primeiro/cliente/diceroll/MainActivity.kt
+++ app/src/main/java/primeiro/cliente/diceroll/MainActivity.kt
@@ -22,16 +22,17 @@ class MainActivity : AppCompatActivity() {
             val firstDice = Dice(6)
             val diceRoll = firstDice.roll()
 
-            when(diceRoll){
-                1 -> image.setImageResource(R.drawable.dice_1)
-                2 -> image.setImageResource(R.drawable.dice_2)
-                3 -> image.setImageResource(R.drawable.dice_3)
-                4 -> image.setImageResource(R.drawable.dice_4)
-                5 -> image.setImageResource(R.drawable.dice_5)
-                6 -> image.setImageResource(R.drawable.dice_6)
+           val result = when(diceRoll){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
             }
-
+            image.setImageResource(result)
         }
+
     }
 
     class Dice(private val numSides: Int) {
